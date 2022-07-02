11807c0ae882e959c03caa47ade2b92d5bd3d778
Soyeb <tssoyeb@gmail.com>
2022-07-02 08:00:28 +0600

    Tested Server side

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+const express = require('express');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Contact form is running and  waiting for data')
+})
+app.listen(port, () => {
+    console.log('Contact form is running on port : ', port);
+})
